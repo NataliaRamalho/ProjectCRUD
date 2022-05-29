dce9978985da2f07e1c4c948e24e8f22ff695219
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-05-29 18:00:16 -0300

    Create GenreEnum

---
+++ src/Enums/GenreEnum.cs
@@ -0,0 +1,19 @@
+namespace ProjectCRUD.src.Enums
+{
+    public enum GenreEnum
+    {
+        Action = 1,
+		Adventure = 2,
+		Comedy = 3,
+		Documentary = 4,
+		Drama = 5,
+		Animation = 6,
+		Western = 7,
+		Fantasy = 8,
+		ScienceFiction = 9,
+		Musical = 10,
+		Romantic  = 11,
+		Thriller = 12,
+        Horror = 13
+    }
+}
